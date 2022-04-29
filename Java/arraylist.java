b64cee8afaab049ccc7988e25905e19257ce867e
VidushAgarwal <vidushagarwal1111@gmail.com>
2022-04-29 19:58:30 +0530

    added comments

--- Java/arraylist.java
+++ Java/arraylist.java
@@ -1,31 +1,39 @@
+//import collections using *
 import java.util.*;
 public class arraylist{
     public static void main(String args[]){
-        System.out.println();
-        Scanner sc=new Scanner(System.in);
+        
+        //create arraylist object, there are two ways to do so, other way is in iterator.java file
         List<Integer> ls=new ArrayList<>();
+
+        //adding elemennts to the arraylist
         for(int i=0;i<4;i++){
             ls.add(i*10);
         }
         System.out.println(ls);
 
-        
+        //adding an element to an index otherwise it will add to the end
         ls.add(1,5);
         System.out.println(ls);
         
+        //removing an integer through value and then changing value of a given index(2) with given element(100)
         ls.remove(Integer.valueOf(20 ));
         ls.set(2 ,100);
         System.out.println(ls);
         
+        //getting element at a specific index
         System.out.println(ls.get(2));
         
+        //checking if list contains specific element
         System.out.println(ls.contains(100));
         
-        //foreach loop!!
+        //foreach loop!! alternative of for loop, in this an element iterates through the list itself
+        //in normal for loop, index iterates
         for (Integer i : ls) {
             System.out.print(i+" ");
         }
         
+        //clears the whole lisst
         ls.clear();
         System.out.println("\n"+ls);
     }
--- Java/iterator.java
+++ Java/iterator.java
@@ -1,14 +1,23 @@
 import java.util.*;
 public class iterator{
     public static void main(String args[]){
+
+        //other way of creating an arraylist
         ArrayList<Integer> ls=new ArrayList<>();
         for(int i=0;i<5;i++)
             ls.add(i*10);
+
+        //creating iterator object, it iterates elements through list instead of index
         Iterator<Integer> it=ls.iterator();
         System.out.println(it.hasNext());
+
+        //running loop in iterator
         while(it.hasNext()){
             System.out.println(it.next());
         }
+
+        //.next shifts iterator to next element thus moving one step ahead
+        //after while loop has confirmed that there is no next element .hasnext now will print false
         System.out.println(it.hasNext());
         System.out.println();
     }
