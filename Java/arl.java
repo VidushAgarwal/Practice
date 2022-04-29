e1ccfad6c04599c445e76db58e0893f723d09af9
VidushAgarwal <vidushagarwal1111@gmail.com>
2022-04-29 19:34:03 +0530

    arraylist

--- Java/arl.java
+++ Java/arl.java
@@ -8,13 +8,23 @@ public class arl{
             ls.add(i*10);
         }
         System.out.println(ls);
+
         ls.add(1,5);
         System.out.println(ls);
+        
         ls.remove(Integer.valueOf(20 ));
         ls.set(2 ,100);
         System.out.println(ls);
+        
+        System.out.println(ls.get(2));
+        
         System.out.println(ls.contains(100));
+        
+        for (Integer i : ls) {
+            System.out.print(i+" ");
+        }
+        
         ls.clear();
-        System.out.println(ls);
+        System.out.println("\n"+ls);
     }
 }
